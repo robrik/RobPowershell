0dcbe6328e99ff9999c4fe5288e9217612a8e2e8
Robert Björklund <deliro257@gmail.com>
2022-09-30 19:47:07 +0300

    fixed RobGitCheckCurrentBranchIsMainBranch

--- RobGitUtils.psm1
+++ RobGitUtils.psm1
@@ -1,7 +1,10 @@
 function RobGitCheckCurrentBranchIsMainBranch {
     $currentBranch = git rev-parse --abbrev-ref HEAD
+ 
     $headBranch = (git remote show origin) -match 'HEAD branch:' -replace 'HEAD branch: '
-    return $currentBranch -eq $targetBranch
+    $headBranch = $headBranch -replace '\s',''
+
+    return $currentBranch -like $headBranch
 }
 
 Export-ModuleMember -Function RobGitCheckCurrentBranchIsMainBranch
